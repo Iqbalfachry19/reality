44baa06f8e716a60c76de8956b0e2bdc9bfaab34
iqbal <ifachry@rocketmail.com>
2023-08-03 14:57:46 +0700

    Initial Commit

---
+++ reality/GameViewController.swift
@@ -0,0 +1,24 @@
+//
+//  GameViewController.swift
+//  reality
+//
+//  Created by Iqbal Fachry on 03/08/23.
+//
+
+import UIKit
+import RealityKit
+
+class GameViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
